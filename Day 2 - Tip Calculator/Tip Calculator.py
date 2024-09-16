0d99b1006cad5364ed05d0e6e7258ef7ac7f5b5e
Tharun Uppala <102760814+TharunGit220@users.noreply.github.com>
2024-09-16 16:17:15 +0100

    Delete Day 2 - Tip Calculator directory

--- Day 2 - Tip Calculator/Tip Calculator.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the Tip Calculator.\n")
-
-bill = float(input("What was the total bill? "))
-tip = float(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100 + 1
-n_people = int(input("How many people to split the bill? "))
-
-amount_per_person = bill * tip / n_people
-
-print(f"Each person should pay: ${amount_per_person:.2f}")
